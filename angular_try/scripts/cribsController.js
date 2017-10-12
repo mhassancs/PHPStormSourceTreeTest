190355adcfdc01c4417d15fa01ae79fa7e4d2aa2
lavexy6619 <lavexy6619@lavexy6619deMacBook-Pro.local>
2017-10-12 12:42:30 -0400

    angular_try is staff about angularjs packages.

---
+++ angular_try/scripts/cribsController.js
@@ -0,0 +1,16 @@
+ angular
+ 	.module('angular_Cribs')
+ 	.controller('angular_Controller', function($scope, cribsFactory) {
+
+	$scope.residences;
+
+	cribsFactory.get_Cribs().then(function(response) {
+			$scope.residences = response.data;
+		}).catch(function(response) {
+			console.error('Recidences error', response.status, response.data);
+		})
+		.finally(function() {
+			console.log("finally finished gists");
+		});
+
+ 	});
