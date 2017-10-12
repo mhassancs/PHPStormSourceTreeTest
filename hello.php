2ac660e41d8f11a0bbefca90ce9b80ed4de01fb1
MohammedHassan\Mohammed Hassan <mhassan@drew.edu>
2017-10-12 00:03:05 -0400

    This is a Test. Looks like I don't need SourceTree to do this...

---
+++ hello.php
@@ -0,0 +1,21 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+	<form method="post" action="">
+    <input type="text" name="something" value="" />
+    <input type="submit" name="submit" />
+  </form>
+
+<?php
+if(isset($_POST['submit'])) {
+  echo 'You entered: ', htmlspecialchars($_POST['something']);
+}
+?>
+ <?php 
+ $name = 'Mohammed';
+ echo '<p>Hello, ', $name, '</p>'; 
+ ?> 
+ </body>
+</html>
