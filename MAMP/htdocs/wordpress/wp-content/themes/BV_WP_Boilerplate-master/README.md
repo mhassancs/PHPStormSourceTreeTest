671824a651e25dba9d45146dc54e89fe676b8419
MohammedHassan\Mohammed Hassan <mhassan@drew.edu>
2017-11-04 14:35:16 -0400

    Removing unneeded MAMP folder, only need wordpress

--- MAMP/htdocs/wordpress/wp-content/themes/BV_WP_Boilerplate-master/README.md
+++
@@ -1,4 +0,0 @@
-# BrandingVerticals Wordpress Boilerplate.
-
-
-
--- MAMP/htdocs/wordpress/wp-content/themes/Sensor_Products-master/inc/component_includes/home-content-txt.php
+++
@@ -1,8 +0,0 @@
-<div class="contentBox">
-  <?php if(have_posts() ) : while(have_posts() ) : the_post(); ?>
-    <div class="<?php post_class(); ?>">
-      <div class="the-content"><?php the_content(); ?></div>
-    </div>
-  <?php endwhile ?>
-  <?php endif ?>
-</div>
